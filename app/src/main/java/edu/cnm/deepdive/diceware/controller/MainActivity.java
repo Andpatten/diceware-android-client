4b2aacf492b8c011c96559ba54b1dc0aacf065fd
Andrew Patten <andpatten@gmail.com>
2019-10-29 16:50:29 -0600

    added a waiting function.

--- app/src/main/java/edu/cnm/deepdive/diceware/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/diceware/controller/MainActivity.java
@@ -3,6 +3,7 @@ package edu.cnm.deepdive.diceware.controller;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ProgressBar;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -14,7 +15,6 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import edu.cnm.deepdive.diceware.R;
-import edu.cnm.deepdive.diceware.controller.LoginActivity;
 import edu.cnm.deepdive.diceware.service.DicewareService;
 import edu.cnm.deepdive.diceware.service.GoogleSignInService;
 import edu.cnm.deepdive.diceware.view.PassphraseAdapter;
@@ -39,30 +39,31 @@ public class MainActivity extends AppCompatActivity {
             .setAction("Action", null).show();
       }
     });
+    ProgressBar waiting = findViewById(R.id.waiting);
     RecyclerView passphraseList = findViewById(R.id.keyword_list);
     MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-    GoogleSignInService.getInstance().getAccount().observe(this, (account -> {
-      viewModel.setAccount(account);
-      viewModel.getPassphrases().observe(this, (passphrases -> {
-        PassphraseAdapter adapter = new PassphraseAdapter(this, passphrases,
-            (view, position, passphrase) -> {
-              // TODO Add code to pop up editor.
-              Log.d("Passphrase click", passphrase.getKey());
-            },
-            (menu, position, passphrase) -> {
-              Log.d("Passphrase context", passphrase.getKey());
-              getMenuInflater().inflate(R.menu.passphrase_context, menu);
-              menu.findItem(R.id.delete_passphrase).setOnMenuItemClickListener(
-                  (item) -> {
-                    Log.d("Delete selected", passphrase.getKey());
-                    // TODO Send request to server to delete passphrase; refresh view.
-                    return true;
-                  });
-            });
-        passphraseList.setAdapter(adapter);
-
-      }));
-    }));
+    GoogleSignInService.getInstance().getAccount().observe(this, (account) ->
+        viewModel.setAccount(account));
+    viewModel.getPassphrases().observe(this, (passphrases) -> {
+      PassphraseAdapter adapter = new PassphraseAdapter(this, passphrases,
+          (view, position, passphrase) -> {
+            // TODO Add code to pop up editor.
+            Log.d("Passphrase click", passphrase.getKey());
+          },
+          (menu, position, passphrase) -> {
+            Log.d("Passphrase context", passphrase.getKey());
+            getMenuInflater().inflate(R.menu.passphrase_context, menu);
+            menu.findItem(R.id.delete_passphrase).setOnMenuItemClickListener(
+                (item) -> {
+                  Log.d("Delete selected", passphrase.getKey());
+                  waiting.setVisibility(View.VISIBLE);
+                  viewModel.deletePassphrase(passphrase);
+                  return true;
+                });
+          });
+      passphraseList.setAdapter(adapter);
+      waiting.setVisibility(View.GONE);
+    });
   }
 
   @Override
